0f8a84fbe01df9afedd2ef48ceebba7253a8cb4d
daniel.igor.rodrigues <daniel.igor.rodrigues@gmail.com>
2024-08-26 23:37:07 -0300

    inclusão do primeiro teste getProducts

---
+++ tests/getProdutos.spec.js
@@ -0,0 +1,57 @@
+const { login, addProduct, getProduct, deleteProductByID, ensureProducts, findProduct } = require('./helpers/helpers');
+const data = require('./fixtures/product.json');
+jest.setTimeout(30000); // Aumenta o timeout global para 30 segundos
+describe('GET /produtos', () => {
+  let token;
+  let registredProduct;
+
+  beforeAll(async () => {
+    token = await login(); // Obtem token para autenticação
+
+    await ensureProducts(token, data.NewProduct); // Exclui qualquer produto previamente cadastrado com os mesmos dados
+
+    registredProduct = await addProduct(token, data.NewProduct); // Cadastro do produto usando os dados do JSON
+  });
+
+  afterAll(async () => {
+    await deleteProductByID(token, registredProduct._id); // Exclui o produto cadastrado após os testes
+  });
+
+  test('Deve retornar status 200 e uma lista de produtos', async () => {
+    const response = await getProduct(); // Faz a requisição GET para o endpoint /produtos
+
+    expect(response.status).toBe(200); // Verifica se o status code é 200
+    expect(Array.isArray(response.body.produtos)).toBe(true); // Verifica se 'produtos' é um array
+  });
+
+    test('Deve verificar que a quantidade corresponde ao número de produtos', async () => {
+    const response = await getProduct();
+
+    expect(response.body.quantidade).toBe(response.body.produtos.length); // Verifica se a informação 'quantidade' é igual ao número de produtos listados
+  });
+
+  test('Os produtos devem conter campos válidos', async () => {
+    const response = await getProduct();
+
+    // Valida sobre cada produto no array e verifica se todos as propriedades de produtos estão presentes
+    response.body.produtos.forEach(produto => {
+      expect(produto).toHaveProperty('nome'); // Verifica se o campo 'nome' está presente
+      expect(produto).toHaveProperty('preco'); // Verifica se o campo 'preco' está presente
+      expect(produto).toHaveProperty('descricao'); // Verifica se o campo 'descricao' está presente
+      expect(produto).toHaveProperty('quantidade'); // Verifica se o campo 'quantidade' está presente
+      expect(produto).toHaveProperty('_id'); // Verifica se o campo '_id' está presente
+    });
+  });
+
+  test('Deve conter o produto recém-cadastrado na lista de produtos', async () => {
+    const product = await getProduct();
+
+    const productFinded = await findProduct(product.body.produtos, registredProduct._id); // Procura no array retornado o produto recém-cadastrado
+
+    expect(productFinded).toBeDefined(); // Verifica se o produto foi encontrado (deve ser definido)
+    expect(productFinded.nome).toBe(data.NewProduct.nome); // Verifica se o nome do produto encontrado corresponde ao nome do produto cadastrado
+    expect(productFinded.preco).toBe(data.NewProduct.preco); // Verifica se o preço do produto encontrado corresponde ao preço cadastrado
+    expect(productFinded.descricao).toBe(data.NewProduct.descricao); // Verifica se a descrição do produto encontrado corresponde à descrição cadastrada
+    expect(productFinded.quantidade).toBe(data.NewProduct.quantidade); // Verifica se a quantidade do produto encontrado corresponde à quantidade cadastrada
+  });
+});
---
+++ tests/helpers/helpers.js
@@ -0,0 +1,59 @@
+const request = require('supertest');
+const baseUrl = 'https://serverest.dev';
+
+// Função para fazer login e retornar o token de autenticação
+async function login() {
+    const loginResponse = await request(baseUrl)
+        .post('/login')
+        .send({ email: 'fulano@qa.com', password: 'teste' });
+
+    return loginResponse.body.authorization;
+}
+
+// Função para cadastrar um produto e retornar o produto cadastrado
+async function addProduct(token, produto) {
+    const response = await request(baseUrl)
+        .post('/produtos')
+        .set('Authorization', token)
+        .send(produto);
+
+    return response.body;
+}
+
+// Função para obter a lista de produtos
+async function getProduct() {
+    const response = await request(baseUrl).get('/produtos');
+
+    return response;
+}
+
+// Função para excluir um produto por ID
+async function deleteProductByID(token, productID) {
+    await request(baseUrl)
+        .delete(`/produtos/${productID}`)
+        .set('Authorization', token);
+}
+
+// Função para excluir produtos com os mesmos dados do produto que será cadastrado
+async function ensureProducts(token, newProduct) {
+    const product = await getProduct();
+    const duplicateProducts = product.body.produtos.filter(p => p.nome === newProduct.nome);
+
+    for (const duplicateProduct of duplicateProducts) {
+        await deleteProductByID(token, duplicateProduct._id);
+    }
+}
+
+async function findProduct(product, id) {
+    return product.find(produto => produto._id === id);
+  }
+  
+
+module.exports = {
+    login,
+    addProduct,
+    getProduct,
+    deleteProductByID,
+    ensureProducts,
+    findProduct,
+};
